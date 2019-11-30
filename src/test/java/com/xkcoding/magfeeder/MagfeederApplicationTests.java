5572b88eb44fded2a2dffdb7ee364bc426eaf459
Yangkai.Shen <237497819@qq.com>
2019-11-30 17:54:01 +0800

    :tada: 初始化提交

---
+++ src/test/java/com/xkcoding/magfeeder/MagfeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xkcoding.magfeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagfeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
